18ae8a5263b67aeb386da352a06ba1257309f70b
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2023-01-02 17:28:40 +0100

    Update app.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h4>with ease!</h4>
       </header>
       <MainContent />
     </>
