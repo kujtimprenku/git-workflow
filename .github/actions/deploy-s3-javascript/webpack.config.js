241feadf6757732e4e773626273b38c16dd6b501
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2023-01-04 22:03:22 +0100

    Use webpack5 to bundle the JS action and remove node_modules.

---
+++ .github/actions/deploy-s3-javascript/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+  },
+};
